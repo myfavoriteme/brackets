9221f315a6436d698c65abdcbcd8fb223b2bdef1
Vladislav Kakorin <vlad.kakorin03@gmail.com>
2023-01-19 20:18:01 +0300

    started this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // let out = '';
+  let arr = str.split('');
+  while(arr.length >= 2){
+    for (let i = 0; i < bracketsConfig.length; i++){
+      let f1 = arr.indexOf(bracketsConfig[i][0]);
+      let f2 = arr.indexOf(bracketsConfig[i][1]);
+      if (((f1 >= 0) && (f2 >= 0)) && (f2 > f1)) {
+        arr.splice(arr.indexOf(bracketsConfig[i][0]), 1);
+        arr.splice(arr.indexOf(bracketsConfig[i][1]), 1);
+      }
+      
+    }
+  }
+  
+
+
+  return arr.length == 0;
 }
